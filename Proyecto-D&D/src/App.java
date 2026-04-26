6c8c7fb8657139dcc98b8c23c3539bc0db125053
usr3745-sys <usr3745@salesianos-lacuesta.net>
2026-04-26 18:05:40 +0200

    refactor: clase app terminada, creados los personajes y el contador

--- Proyecto-D&D/src/App.java
+++ Proyecto-D&D/src/App.java
@@ -1,5 +1,16 @@
+import net.salesianos.guerrero.Guerrero;
+import net.salesianos.mago.Mago;
+import net.salesianos.personaje.Personaje;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Guerrero g1 = new Guerrero("Aragorn", 10, 100, "Espada de Andúril");
+        Mago m1 = new Mago("Gandalf", 12, 80, "Fuego de Anor");
+
+        g1.atacar();
+        m1.atacar();
+
+        // Acceso estático (PDF Estáticos)
+        System.out.println("Total de personajes creados: " + Personaje.totalPersonajes);
     }
 }
