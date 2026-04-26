f59b7976f0a56dc4c7d13d35cbe48493bfdc2600
usr3745-sys <usr3745@salesianos-lacuesta.net>
2026-04-26 18:00:47 +0200

    refactor: añadida la clase mago y sobrescrito el metodo de atacar

--- Proyecto-D&D/src/net/salesianos/mago/Mago.java
+++ Proyecto-D&D/src/net/salesianos/mago/Mago.java
@@ -1,5 +1,17 @@
 package net.salesianos.mago;
 
-public class Mago {
-    
+import net.salesianos.personaje.Personaje;
+
+public class Mago extends Personaje {
+    private String hechizo;
+
+    public Mago(String nombre, int nivel, int puntosVida, String hechizo) {
+        super(nombre, nivel, puntosVida);
+        this.hechizo = hechizo;
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println(nombre + " lanza el hechizo " + hechizo + " consumiendo maná.");
+    }
 }
