bf09edce72e4328963878bf70cc2a39f0b593e75
usr3745-sys <usr3745@salesianos-lacuesta.net>
2026-04-26 17:51:47 +0200

    refactor: añadido base de la clase personaje con contador estatico

--- Proyecto-D&D/src/net/salesianos/personaje/Personaje.java
+++ Proyecto-D&D/src/net/salesianos/personaje/Personaje.java
@@ -1,5 +1,20 @@
 package net.salesianos.personaje;
 
 public class Personaje {
-    
+    protected String nombre;
+    protected int nivel;
+    protected int puntosVida;
+
+    public static int totalPersonajes = 0;
+
+    public Personaje(String nombre, int nivel, int puntosVida) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.puntosVida = puntosVida;
+        totalPersonajes++; 
+    }
+
+    public void atacar() {
+        System.out.println(nombre + " realiza un ataque base.");
+    }
 }
