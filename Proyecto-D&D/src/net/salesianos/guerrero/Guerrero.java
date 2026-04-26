e17b569c77576b2c6eac4756c2017ee7695cefb5
usr3745-sys <usr3745@salesianos-lacuesta.net>
2026-04-26 17:56:47 +0200

    refactor: añadida clase guerrero con herencia de la clase personaje

--- Proyecto-D&D/src/net/salesianos/guerrero/Guerrero.java
+++ Proyecto-D&D/src/net/salesianos/guerrero/Guerrero.java
@@ -1,5 +1,16 @@
 package net.salesianos.guerrero;
+import net.salesianos.personaje.Personaje;
 
-public class Guerrero {
-    
+public class Guerrero extends Personaje {
+    private String arma;
+
+    public Guerrero(String nombre, int nivel, int puntosVida, String arma) {
+        super(nombre, nivel, puntosVida); 
+        this.arma = arma;
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println(nombre + " ataca con su " + arma + " causando un gran daño físico.");
+    }
 }
